aa89a810396b602e2ea6c8946ce6893c0de4c5a4
mikegv <xmik3yvx@live.com>
2022-05-31 16:27:27 -0700

    hero

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import './hero.css'
+import WorkingImage from '../../images/illustration-working.svg'
+import { StyledButton } from '../styledComponents/styledComponents';
 
 const Hero = () => {
     return (
-        <div>
-            
-More than just shorter links
+        <div className='hero'>
+            <div className='heroLeft'>
+<h1>More than just <br />shorter links</h1>
 
-Build your brand’s recognition and get detailed insights 
+Build your brand’s recognition and get detailed 
+<br />insights 
 on how your links are performing.
 
-Get Started
+<StyledButton size={'large'}>Get Started</StyledButton>
+</div>
+<div className='heroRight'>
+    <img src={WorkingImage} alt='person working' />
+</div>
         </div>
     );
 };
--- src/components/styledComponents/styledComponents.js
+++ src/components/styledComponents/styledComponents.js
@@ -2,13 +2,13 @@ import styled from 'styled-components'
 
 export const StyledButton = styled.button`
     background-color: hsl(180, 66%, 49%);
-    padding: 12px 20px;
-    border-radius: 25px;
+    
     border: none;
     color: white;
     font-weight: 600;
-    font-size: 15px;
-
+    
+    
+    ${props => props.size === 'large' ? 'padding: 15px 40px; font-size: 25px;border-radius: 50px;margin-top: 30px;' : 'padding: 12px 20px; font-size: 15px;border-radius: 25px;'}
     &:hover{
         cursor: pointer;
         background-color: hsl(180, 80%, 67%);
